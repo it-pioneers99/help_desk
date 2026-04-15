7a98b61e612176e31c574d248739c0e4d9fedcf4
it-pioneers99 <it@pioneersholding.ae>
2026-04-15 10:21:00 +0300

    first-upl

---
+++ help_desk/help_desk/doctype/helpdesk_order/helpdesk_order.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Pioneer Holding and contributors
+# For license information, please see license.txt
+
+from frappe.model.document import Document
+
+
+class HelpDeskOrder(Document):
+	pass
+
